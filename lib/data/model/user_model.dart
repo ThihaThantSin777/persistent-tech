6dcab24b034dcf7bc208b07cfe205dde0090f455
Thiha Thant Sin <thantsin7755@gmail.com>
2023-10-25 16:37:41 +0630

    Close comment

--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -21,14 +21,12 @@ class UserModel {
       .startWith(UserDAOHiveImpl().getUserDataStream)
       .map((event) => _userDAO.getUser(1));
 
-
-  void closeStream() {
-    _userStream.close();
-  }
-
+  //
+  // void closeStream() {
+  //   _userStream.close();
+  // }
 
   Future<void> saveUserWithAsync({bool isUsedSharePreferences = false}) async {
-
     await Future.delayed(const Duration(seconds: 3));
     final userData = UserVO(1, "Mg Mg", 21);
     save(userData, isAddedUserStream: isUsedSharePreferences);
