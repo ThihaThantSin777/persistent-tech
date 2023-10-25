896a06c8b0375dc46f31e03dbe3fc5f23fc11ec5
Thiha Thant Sin <thantsin7755@gmail.com>
2023-10-25 16:27:05 +0630

    hive reactive impl

--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:persistent/data/vo/user_vo.dart';
 import 'package:persistent/persistent/dao/user_dao/user_dao.dart';
 import 'package:persistent/persistent/dao/user_dao/user_dao_hive_impl.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 class UserModel {
   UserModel._();
@@ -15,15 +16,19 @@ class UserModel {
 
   final UserDAO _userDAO = UserDAOHiveImpl();
 
-  Stream<UserVO?> get getUserStream => UserDAOHiveImpl().getUserStream;
+  Stream<UserVO?> get getUserStream => UserDAOHiveImpl()
+      .watchUserBox()
+      .startWith(UserDAOHiveImpl().getUserDataStream)
+      .map((event) => _userDAO.getUser(1));
 
-  Future<void> saveUserWithAsync() async {
+  Future<void> saveUserWithAsync({bool isUsedSharePreferences = false}) async {
     await Future.delayed(const Duration(seconds: 3));
     final userData = UserVO(1, "Mg Mg", 21);
-    save(userData);
+    save(userData, isAddedUserStream: isUsedSharePreferences);
   }
 
-  void save(UserVO userVO) => _userDAO.save(userVO);
+  void save(UserVO userVO, {bool isAddedUserStream = false}) =>
+      _userDAO.save(userVO, isAddedUserStream: isAddedUserStream);
 
   UserVO? get getUser => _userDAO.getUser(1);
 }
